6a3e9f04d3434562809886207ad9cf01b3b880bb
anubhav-srivastava6183 <anubhav.srivastava@iceservices.com>
2026-02-11 16:39:59 +0100

    updating application

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Kubernetes!"
+    return "Hi, Kubernetes!"
 
 if __name__ == "__main__":
     # listen on 0.0.0.0 so Kubernetes can access it
